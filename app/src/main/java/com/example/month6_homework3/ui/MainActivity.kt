787508f35c0d5151d42c155c6f81ff1d016df406
aidar <aidar.azhibraimov@mail.ru>
2022-03-05 10:01:14 +0600

    add

---
+++ app/src/main/java/com/example/month6_homework3/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.month6_homework3.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.month6_homework3.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/month6_homework3/ui/seconde/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.example.month6_homework3.ui.seconde
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.month6_homework3.R
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
